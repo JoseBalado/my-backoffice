1a58f859725edd56aa6538c9f1b0023ad72aaab8
Jose Balado <jose.balado@inflightdublin.com>
2018-01-23 16:18:59 +0000

    Add jsonwebtoken

--- server.mjs
+++ server.mjs
@@ -1,7 +1,26 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const createToken = function (user) {
+  const token = jwt.sign(user, secret)
+  console.log('token Created')
+  return token
+}
+
+const secret = 'secret'
+
+const users = [
+  {
+    name: 'jose',
+    password: 1234
+  }
+]
+
+app.get('/token', (req, res) => {
+  const token = createToken(users[0])
+  res.json({ token: token })
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
