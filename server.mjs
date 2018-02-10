fedaf4eb34e82792c98233feebcee273e7b90524
Jose Balado <jose.balado@gmail.com>
2018-02-10 17:29:39 +0000

    Routes refactor

--- server.mjs
+++ server.mjs
@@ -9,6 +9,7 @@ app.set('trust proxy', 1) // trust first proxy
 app.use(cookieSession({
   name: 'session',
   secret: 'cookie-secret',
+  sameSite: 'lax',
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 
@@ -28,75 +29,76 @@ const users = [
   }
 ]
 
-app.get('/', function (req, res, next) {
+app.get('/api', function (req, res, next) {
   // Update views
   req.session.views = (req.session.views || 0) + 1
 
   // Write response
-  res.end(req.session.views + ' views')
+  res.json(req.session.views + ' views')
 })
 
 app.get('/login', function (req, res, next) {
-  res.end('you are logged')
+  req.session.views = (req.session.views || 0) + 1
+  res.end('you are logged:' + req.session.views + 'views')
 })
 
 app.get('/token', (req, res) => {
   const token = createToken(users[0])
   req.session.token = token
-  res.json('token generated')
+  res.json('Token generated and added to the cookie')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 const permissions = `{
-  "canAccessServerStatus" : true,
-  "canAccessUtility" : true,
-  "canAccessDesigner" : true,
-  "canAccessSynchronise" : true,
-  "canAccessBackup" : true,
-  "canAccessPackage" : true,
-  "canConfigureMenus" : true,
-  "canAccessReports" : true,
-  "canManageRoles" : true,
-  "canViewAllClients" : true,
-  "canConfigureClients" : true,
-  "canCheckContentPackageStatus" : true,
-  "canManageCycles" : true,
-  "canAccessCycles" : true,
-  "canManageFleet" : true,
-  "canAccessFleet" : true,
-  "canConfigureAdvertisement" : true,
-  "canAccessAdvertisement" : true,
-  "canManageECommerce" : true,
-  "canAccessECommerce" : true,
-  "canManageDestination" : true,
-  "canAccessDestination" : true,
-  "canManageDestinationCity" : true,
-  "canAccessDestinationCity" : true,
-  "canWriteMetadata" : true,
-  "canReadMetadata" : true,
-  "name" : "SuperAdmin",
-  "__v" : 0,
-  "canAccessImageRepo" : true,
-  "canReadApplication" : true,
-  "canWriteApplication" : true,
-  "canReadDeliverType" : true,
-  "canWriteDeliverType" : true,
-  "canAccessCityMapGenerator" : true,
-  "canAccessGlobalMapGenerator" : true,
-  "canAccessMenuDesigner" : true,
-  "canAccessSurveyDesigner" : true,
-  "canManageUsers" : true,
-  "canAccessOnboardServer" : true,
-  "canAccessUsers" : true,
-  "canManageOnboardServer" : true,
-  "canAccessFleetManagement" : true,
-  "canAccessFleetStatus" : true,
-  "canManageFleetManagement" : true,
-  "canManageLookupData" : true,
-  "canReadPromotions" : true,
-  "canWritePromotions" : true,
-  "canAccessPromotions" : true,
-  "canAccessDashboard" : true,
-  "canGeneratePromotionsPackage" : true
+  "name": 'Mr Admin',
+  'client': 'Rossya',
+  "canAccessServerStatus": true,
+  "canAccessUtility": true,
+  "canAccessDesigner": true,
+  "canAccessSynchronise": true,
+  "canAccessBackup": true,
+  "canAccessPackage": true,
+  "canConfigureMenus": true,
+  "canAccessReports": true,
+  "canManageRoles": true,
+  "canViewAllClients": true,
+  "canConfigureClients": true,
+  "canCheckContentPackageStatus": true,
+  "canManageCycles": true,
+  "canAccessCycles": true,
+  "canManageFleet": true,
+  "canAccessFleet": true,
+  "canConfigureAdvertisement": true,
+  "canAccessAdvertisement": true,
+  "canManageECommerce": true,
+  "canAccessECommerce": true,
+  "canManageDestination": true,
+  "canAccessDestination": true,
+  "canManageDestinationCity": true,
+  "canAccessDestinationCity": true,
+  "canWriteMetadata": true,
+  "canReadMetadata": true,
+  "canAccessImageRepo": true,
+  "canReadApplication": true,
+  "canWriteApplication": true,
+  "canReadDeliverType": true,
+  "canWriteDeliverType": true,
+  "canAccessCityMapGenerator": true,
+  "canAccessGlobalMapGenerator": true,
+  "canAccessMenuDesigner": true,
+  "canAccessSurveyDesigner": true,
+  "canManageUsers": true,
+  "canAccessOnboardServer": true,
+  "canAccessUsers": true,
+  "canManageOnboardServer": true,
+  "canAccessFleetManagement": true,
+  "canAccessFleetStatus": true,
+  "canManageFleetManagement": true,
+  "canManageLookupData": true,
+  "canReadPromotions": true,
+  "canWritePromotions": true,
+  "canAccessPromotions": true,
+  "canAccessDashboard": true,
+  "canGeneratePromotionsPackage": true
 }`
