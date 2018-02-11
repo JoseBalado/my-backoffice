5aee7206a90cea4d3ed7e98c6c13cf3c8319a2cb
Jose Balado <jose.balado@gmail.com>
2018-02-11 17:10:09 +0000

    Create users.json, permissions.json and POST route

--- routes/login/login.mjs
+++ routes/login/login.mjs
@@ -1,16 +1,12 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 
+import permissions from './permissions.json'
+import user from './user.json'
+
 const router = express.Router()
 
 const secret = 'secret'
-
-const users = [
-  {
-    name: 'jose',
-    password: 1234
-  }
-]
 
 const createToken = function (user) {
   console.log(user)
@@ -21,62 +17,13 @@ const createToken = function (user) {
 
 router.get('/', function (req, res, next) {
   req.session.views = (req.session.views || 0) + 1
-  const token = createToken(users[0])
+  const token = createToken(user)
   req.session.token = token
   res.end('you are logged:' + req.session.views + 'views')
 })
 
-export default router
+router.post('/', function (req, res, next) {
+  res.end('post request received')
+})
 
-const permissions = `{
-  "name": 'Mr Admin',
-  'client': 'Rossya',
-  "canAccessServerStatus": true,
-  "canAccessUtility": true,
-  "canAccessDesigner": true,
-  "canAccessSynchronise": true,
-  "canAccessBackup": true,
-  "canAccessPackage": true,
-  "canConfigureMenus": true,
-  "canAccessReports": true,
-  "canManageRoles": true,
-  "canViewAllClients": true,
-  "canConfigureClients": true,
-  "canCheckContentPackageStatus": true,
-  "canManageCycles": true,
-  "canAccessCycles": true,
-  "canManageFleet": true,
-  "canAccessFleet": true,
-  "canConfigureAdvertisement": true,
-  "canAccessAdvertisement": true,
-  "canManageECommerce": true,
-  "canAccessECommerce": true,
-  "canManageDestination": true,
-  "canAccessDestination": true,
-  "canManageDestinationCity": true,
-  "canAccessDestinationCity": true,
-  "canWriteMetadata": true,
-  "canReadMetadata": true,
-  "canAccessImageRepo": true,
-  "canReadApplication": true,
-  "canWriteApplication": true,
-  "canReadDeliverType": true,
-  "canWriteDeliverType": true,
-  "canAccessCityMapGenerator": true,
-  "canAccessGlobalMapGenerator": true,
-  "canAccessMenuDesigner": true,
-  "canAccessSurveyDesigner": true,
-  "canManageUsers": true,
-  "canAccessOnboardServer": true,
-  "canAccessUsers": true,
-  "canManageOnboardServer": true,
-  "canAccessFleetManagement": true,
-  "canAccessFleetStatus": true,
-  "canManageFleetManagement": true,
-  "canManageLookupData": true,
-  "canReadPromotions": true,
-  "canWritePromotions": true,
-  "canAccessPromotions": true,
-  "canAccessDashboard": true,
-  "canGeneratePromotionsPackage": true
-}`
+export default router
