16742bb17c28a40a86f0107ee241bd62d86bb8b6
Jose Balado <jose.balado@gmail.com>
2018-02-11 02:33:25 +0000

    Create route for login

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,28 +1,7 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+import login from './login/login'
 
 const router = express.Router()
-
-const secret = 'secret'
-
-const users = [
-  {
-    name: 'jose',
-    password: 1234
-  }
-]
-
-const createToken = function (user) {
-  console.log(user)
-  const token = jwt.sign(permissions, secret)
-  console.log('token Created')
-  return token
-}
-
-router.get('/login', function (req, res, next) {
-  req.session.views = (req.session.views || 0) + 1
-  res.end('you are logged:' + req.session.views + 'views')
-})
 
 router.get('/api', function (req, res, next) {
   // Update views
@@ -32,63 +11,6 @@ router.get('/api', function (req, res, next) {
   res.json(req.session.views + ' views')
 })
 
-router.get('/token', (req, res) => {
-  const token = createToken(users[0])
-  req.session.token = token
-  res.json('Token generated and added to the cookie')
-})
+router.use('/login', login)
 
 export default router
-
-const permissions = `{
-  "name": 'Mr Admin',
-  'client': 'Rossya',
-  "canAccessServerStatus": true,
-  "canAccessUtility": true,
-  "canAccessDesigner": true,
-  "canAccessSynchronise": true,
-  "canAccessBackup": true,
-  "canAccessPackage": true,
-  "canConfigureMenus": true,
-  "canAccessReports": true,
-  "canManageRoles": true,
-  "canViewAllClients": true,
-  "canConfigureClients": true,
-  "canCheckContentPackageStatus": true,
-  "canManageCycles": true,
-  "canAccessCycles": true,
-  "canManageFleet": true,
-  "canAccessFleet": true,
-  "canConfigureAdvertisement": true,
-  "canAccessAdvertisement": true,
-  "canManageECommerce": true,
-  "canAccessECommerce": true,
-  "canManageDestination": true,
-  "canAccessDestination": true,
-  "canManageDestinationCity": true,
-  "canAccessDestinationCity": true,
-  "canWriteMetadata": true,
-  "canReadMetadata": true,
-  "canAccessImageRepo": true,
-  "canReadApplication": true,
-  "canWriteApplication": true,
-  "canReadDeliverType": true,
-  "canWriteDeliverType": true,
-  "canAccessCityMapGenerator": true,
-  "canAccessGlobalMapGenerator": true,
-  "canAccessMenuDesigner": true,
-  "canAccessSurveyDesigner": true,
-  "canManageUsers": true,
-  "canAccessOnboardServer": true,
-  "canAccessUsers": true,
-  "canManageOnboardServer": true,
-  "canAccessFleetManagement": true,
-  "canAccessFleetStatus": true,
-  "canManageFleetManagement": true,
-  "canManageLookupData": true,
-  "canReadPromotions": true,
-  "canWritePromotions": true,
-  "canAccessPromotions": true,
-  "canAccessDashboard": true,
-  "canGeneratePromotionsPackage": true
-}`
